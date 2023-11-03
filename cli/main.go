29593f27e8f206c6d9eb26ad5e171ba3500e4256
Austin Raney <aaraney@protonmail.com>
2023-11-03 15:55:36 -0400

    pull from v2.0

--- cli/main.go
+++ cli/main.go
@@ -42,7 +42,7 @@ Usage:
 Examples:
   hfsubset -l divides,nexus        \
            -o ./divides_nexus.gpkg \
-           -r "pre-release"        \
+           -r "v20"                 \
            -t hl_uri                   \
            "Gages-06752260"
 
@@ -106,7 +106,8 @@ func (opts *SubsetRequest) MarshalJSON() ([]byte, error) {
 
 	jsonmap["layers"] = opts.Layers()
 	jsonmap[key] = opts.IDs()
-	jsonmap["version"] = "pre-release"
+    // TODO: use opts.version
+	jsonmap["version"] = "v20" // v20 is v2.0
 	return json.Marshal(jsonmap)
 }
 
